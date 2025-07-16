92f5078398be776a7379a4cd9a90a70798f2fcb4
Mauro <vanillainanutshell@gmail.com>
2025-07-16 11:34:12 -0300

    Updates

---
+++ assets/ProtectedRoute-taMxCbmG.js
@@ -0,0 +1,1 @@
+import{j as e,aA as a}from"./index-Be0rq9yz.js";function r({children:t}){return localStorage.getItem("isAuthenticated")==="true"?t:e.jsx(a,{to:"/login",replace:!0})}export{r as default};
