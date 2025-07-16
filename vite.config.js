85619f99120d60be1539559a00f62b1d14de4d04
Mauro <vanillainanutshell@gmail.com>
2025-07-16 12:17:37 -0300

    fix fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/evolux360-dev/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html')
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
